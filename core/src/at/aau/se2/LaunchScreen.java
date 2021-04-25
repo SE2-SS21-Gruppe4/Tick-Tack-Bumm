f5ea1ab2c345659e5b69a001bee65dc0f8a25d32
DanielFrankl <danielfra@edu.aau.at>
2021-04-25 16:28:29 +0200

    added LaunchScreen as the startscreen

--- core/src/at/aau/se2/LaunchScreen.java
+++ core/src/at/aau/se2/LaunchScreen.java
@@ -2,26 +2,26 @@ package at.aau.se2;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
-public class LaunchScreen extends ApplicationAdapter {
+public class LaunchScreen extends ScreenAdapter {
     SpriteBatch batch;
     Texture img;
     Sprite sprite;
 
-    @Override
-    public void create () {
+    public LaunchScreen(){
         batch = new SpriteBatch();
         img = new Texture("bombeStart.png");
         sprite = new Sprite(img);
     }
 
     @Override
-    public void render () {
+    public void render (float delta) {
         Gdx.gl.glClearColor(.18f, .21f, .32f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
