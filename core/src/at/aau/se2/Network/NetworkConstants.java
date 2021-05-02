43b5f623ae24e2db788176d11bc7fcfaf4ababf6
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2021-05-02 09:57:04 +0200

    Add network classes

---
+++ core/src/at/aau/se2/Network/NetworkConstants.java
@@ -0,0 +1,6 @@
+package at.aau.se2.Network;
+
+public class NetworkConstants {
+    public static final int TCP_PORT = 2021;
+    public static final int TIMEOUT = 5000;
+}
---
+++ core/src/at/aau/se2/Network/messages/Messages.java
@@ -0,0 +1,10 @@
+package at.aau.se2.Network.messages;
+
+public class Messages {
+    public static class SomeRequest {
+        public String text;
+    }
+    public static class SomeResponse {
+        public String text;
+    }
+}
