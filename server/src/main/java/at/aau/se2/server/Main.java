da472015a8a419c9bdec64a7c14eb44f45fcaeff
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2021-05-02 12:16:12 +0200

    Start server in Main

--- server/src/main/java/at/aau/se2/server/Main.java
+++ server/src/main/java/at/aau/se2/server/Main.java
@@ -1,7 +1,14 @@
 package at.aau.se2.server;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         ServerMain server = new ServerMain();
+        try {
+            server.startServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
