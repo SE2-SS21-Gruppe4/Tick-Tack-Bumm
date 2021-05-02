fbc424dd599219d1a4d0a02355b4916acda33612
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2021-05-02 15:07:15 +0200

    Refactor request logic; remove response display; add loading message

--- core/src/at/aau/se2/screens/LoadingScreen.java
+++ core/src/at/aau/se2/screens/LoadingScreen.java
@@ -1,5 +1,6 @@
 package at.aau.se2.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.Batch;
@@ -7,61 +8,29 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryonet.Client;
-import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.Listener;
-
-import java.io.IOException;
 
 import at.aau.se2.TickTackBummGame;
-import at.aau.se2.network.NetworkConstants;
-import at.aau.se2.network.messages.Messages;
+import at.aau.se2.client.NetworkClient;
 
 public class LoadingScreen extends ScreenAdapter {
     private final TickTackBummGame game;
     private final AssetManager assetManager;
-    private final Client client;
-    private String serverResponse = "";
-    BitmapFont font;
-    private Batch batch;
+    private final NetworkClient networkClient;
+    private final BitmapFont font;
+    private final Batch batch;
 
     public LoadingScreen() {
         this.game = TickTackBummGame.getTickTackBummGame();
+        this.networkClient = game.getNetworkClient();
         this.assetManager = game.getManager();
 
         this.assetManager.load("explosion.atlas", TextureAtlas.class);
 
-        batch = new SpriteBatch();
-        font = new BitmapFont();
+        this.batch = new SpriteBatch();
+        this.font = new BitmapFont();
 
-        // TODO: refactor
-        this.client = this.game.getClient();
-
-        Kryo kryo = this.client.getKryo();
-        kryo.register(Messages.SomeRequest.class);
-        kryo.register(Messages.SomeResponse.class);
-
-        client.start();
-        try {
-            client.connect(NetworkConstants.TIMEOUT, "localhost", NetworkConstants.TCP_PORT);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Messages.SomeRequest request = new Messages.SomeRequest();
-        request.text = "Here is a request";
-        client.sendTCP(request);
-
-        client.addListener(new Listener() {
-            public void received(Connection connection, Object object) {
-                if (object instanceof Messages.SomeResponse) {
-                    Messages.SomeResponse response = (Messages.SomeResponse) object;
-                    System.out.println(response.text);
-                    serverResponse = response.text;
-                }
-            }
-        });
+        this.networkClient.tryConnectClient();
+        this.networkClient.getMessageSender().sendSomeRequest("Here is a request!");
     }
 
     @Override
@@ -69,7 +38,12 @@ public class LoadingScreen extends ScreenAdapter {
         ScreenUtils.clear(1.0f, 0, 1.0f, 1);
 
         batch.begin();
-        font.draw(batch, serverResponse, 800, 200);
+        font.draw(
+                batch,
+                "LOADING ASSETS...",
+                (float) (Gdx.graphics.getWidth() / 2) - 100,
+                (float) Gdx.graphics.getHeight() / 2
+        );
         batch.end();
 
         if (assetManager.update()) {
