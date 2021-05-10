6614cc9dc46c04909a2135b4b4e7cc444dc3ed40
DanielFrankl <danielfra@edu.aau.at>
2021-05-10 19:36:19 +0200

    add classes for Player UI

---
+++ core/src/main/java/se2/ticktackbumm/core/models/Score.java
@@ -0,0 +1,13 @@
+package se2.ticktackbumm.core.models;
+
+import se2.ticktackbumm.core.models.attributes.Position;
+
+public class Score extends Position{
+    Position position;
+
+    public Score(Position position) {
+        this.position = position;
+    }
+
+
+}
