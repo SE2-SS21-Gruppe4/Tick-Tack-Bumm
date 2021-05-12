5dbefb3402aa3eab4facff3ef593d2565ffbfc35
Jusuf <jusuftopic@yahoo.com>
2021-05-12 14:42:43 +0200

    With every open for card schuffle the array list and get another word

--- core/src/main/java/se2/ticktackbumm/core/models/CardLayout/Card.java
+++ core/src/main/java/se2/ticktackbumm/core/models/CardLayout/Card.java
@@ -1,6 +1,7 @@
 package se2.ticktackbumm.core.models.CardLayout;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
-public class Card {
+public class Card implements InputProcessor {
     private ArrayList<String> carddeck;
 
     public Stage stage;
@@ -50,8 +51,7 @@ public class Card {
 
     public void render(){
         if (!isRevealed){
-         //   drawBackSide();
-            drawFontSide();
+            drawBackSide();
         }
         else {
             drawFontSide();
@@ -85,6 +85,7 @@ public class Card {
     public void openCard(){
         if (!isRevealed){
             isRevealed = true;
+            this.ranodmWord = getRandomWord();
         }
     }
 
@@ -120,4 +121,43 @@ public class Card {
         isRevealed = revealed;
     }
 
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
 }
