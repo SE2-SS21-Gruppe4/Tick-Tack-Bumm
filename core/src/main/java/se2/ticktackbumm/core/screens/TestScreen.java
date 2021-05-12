8d66b144b9b3df357733c2a90f157357c2f3c0ce
Jusuf <jusuftopic@yahoo.com>
2021-05-12 14:38:16 +0200

    Card succesefull drawed

--- core/src/main/java/se2/ticktackbumm/core/screens/TestScreen.java
+++ core/src/main/java/se2/ticktackbumm/core/screens/TestScreen.java
@@ -1,19 +1,26 @@
 package se2.ticktackbumm.core.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import se2.ticktackbumm.core.TickTackBummGame;
+import se2.ticktackbumm.core.models.CardLayout.Card;
 
 public class TestScreen implements Screen {
 
     private final TickTackBummGame game;
     private SpriteBatch batch;
 
+    private Card card;
+
     public TestScreen(){
         game = TickTackBummGame.getTickTackBummGame();
         batch = game.getBatch();
+
+        card = new Card(batch);
     }
 
 
@@ -24,7 +31,10 @@ public class TestScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        card.render();
     }
 
     @Override
@@ -49,6 +59,6 @@ public class TestScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        card.stage.dispose();
     }
 }
