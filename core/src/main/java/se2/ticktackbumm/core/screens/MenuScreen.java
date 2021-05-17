470017c55a76d65e41a62cf77ef54655098996ca
JudithLagger <judithla@edu.aau.at>
2021-05-17 22:22:28 +0200

    modified MenuScreen

--- core/src/main/java/se2/ticktackbumm/core/screens/MenuScreen.java
+++ core/src/main/java/se2/ticktackbumm/core/screens/MenuScreen.java
@@ -2,11 +2,13 @@ package se2.ticktackbumm.core.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -20,7 +22,7 @@ import se2.ticktackbumm.core.assets.Explosion;
 public class MenuScreen extends ScreenAdapter {
 
     // Button constants
-    private final float BUTTON_WIDTH = 350f;
+    private final float BUTTON_WIDTH = 500f;
     private final float BUTTON_HEIGHT = 80f;
 
     // TickTackBumm resources
@@ -36,33 +38,47 @@ public class MenuScreen extends ScreenAdapter {
     private final Skin skin;
     private final TextButton playButton;
     private final TextButton rulesButton;
-    private final TextButton exitButton;
+    private final TextButton quitButton;
     private final Table menuButtonTable;
+    private final Image backgroundImage;
+    private final Texture textrueImage;
+    private final AssetManager assetManager;
+
 
 
     public MenuScreen() {
-        game = TickTackBummGame.getTickTackBummGame();
-        camera = TickTackBummGame.getGameCamera();
-        batch = game.getBatch();
+        this.game = TickTackBummGame.getTickTackBummGame();
+        this.camera = TickTackBummGame.getGameCamera();
+        this.batch = game.getBatch();
+        this.assetManager=game.getManager();
+
+
 
         explosion = new Explosion();
-
+//scene2d UI
         stage = new Stage(new FitViewport(TickTackBummGame.WIDTH, TickTackBummGame.HEIGHT));
         skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
         Gdx.input.setInputProcessor(stage);
 
+//MenuButtons
         playButton = new TextButton("PLAY", skin);
         rulesButton = new TextButton("RULES", skin);
-        exitButton = new TextButton("EXIT", skin);
+        quitButton = new TextButton("QUIT", skin);
 
-        playButton.getLabel().setFontScale(3);
-        rulesButton.getLabel().setFontScale(3);
-        exitButton.getLabel().setFontScale(3);
+        playButton.getLabel().setFontScale(4);
+        rulesButton.getLabel().setFontScale(4);
+        quitButton.getLabel().setFontScale(4);
 
         menuButtonTable = new Table();
         menuButtonTable.setWidth(stage.getWidth());
         menuButtonTable.setHeight(stage.getHeight());
         menuButtonTable.align(Align.center);
+
+
+        textrueImage = new Texture("menuscreen.png");
+        backgroundImage=new Image(textrueImage);
+
+
 
         // TODO: player login, waiting for other player? (WaitingScreen?)
         playButton.addListener(new ClickListener() {
@@ -74,7 +90,7 @@ public class MenuScreen extends ScreenAdapter {
         });
         // TODO: add RulesScreen
         rulesButton.addListener(new ClickListener());
-        exitButton.addListener(new ClickListener() {
+        quitButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 Gdx.app.exit();
@@ -85,9 +101,11 @@ public class MenuScreen extends ScreenAdapter {
         menuButtonTable.row();
         menuButtonTable.add(rulesButton).padBottom(50f).width(BUTTON_WIDTH).height(BUTTON_HEIGHT);
         menuButtonTable.row();
-        menuButtonTable.add(exitButton).padBottom(50f).width(BUTTON_WIDTH).height(BUTTON_HEIGHT);
+        menuButtonTable.add(quitButton).padBottom(50f).width(BUTTON_WIDTH).height(BUTTON_HEIGHT);
 
+        stage.addActor(backgroundImage);
         stage.addActor(menuButtonTable);
+
     }
 
     @Override
