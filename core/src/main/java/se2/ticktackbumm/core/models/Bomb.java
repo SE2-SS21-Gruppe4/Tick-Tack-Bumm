4034ee99756555eb779684bb5a2eedc2d01a093d
Jusuf <jusuftopic@yahoo.com>
2021-05-19 16:23:42 +0200

    Bomb drawn on screen

--- core/src/main/java/se2/ticktackbumm/core/models/Bomb.java
+++ core/src/main/java/se2/ticktackbumm/core/models/Bomb.java
@@ -1,28 +1,33 @@
 package se2.ticktackbumm.core.models;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 import se2.ticktackbumm.core.TickTackBummGame;
 
-public class Bomb extends Actor {
+public class Bomb {
 
-    private TickTackBummGame game;
-    private AssetManager assetManager;
+    private Stage stage;
+
     private Texture bombTexture;
     private Image bombImage;
 
     public Bomb(){
-        game = TickTackBummGame.getTickTackBummGame();
+        stage = new Stage();
 
-        assetManager = game.getManager();
-        assetManager.load("assets/bomb/bomb.png",Texture.class);
-        assetManager.finishLoading();
-
-        bombTexture = assetManager.get("assets/bomb/bomb.png",Texture.class);
+        bombTexture = new Texture("bomb/bomb.png");
         bombImage = new Image(bombTexture);
+
+    }
+
+    public void drawBomb(){
+        bombImage.setBounds(20,40,50,60);
+        stage.addActor(bombImage);
+        stage.draw();
     }
 
 
