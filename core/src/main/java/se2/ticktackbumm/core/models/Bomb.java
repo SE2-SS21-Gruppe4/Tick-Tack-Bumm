03d34934451a83261c56dab35ec7d4b722e49f60
Jusuf <jusuftopic@yahoo.com>
2021-05-19 15:50:28 +0200

    loaded textures with AssetManager

--- core/src/main/java/se2/ticktackbumm/core/models/Bomb.java
+++ core/src/main/java/se2/ticktackbumm/core/models/Bomb.java
@@ -1,4 +1,29 @@
 package se2.ticktackbumm.core.models;
 
-public class Bomb {
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+import se2.ticktackbumm.core.TickTackBummGame;
+
+public class Bomb extends Actor {
+
+    private TickTackBummGame game;
+    private AssetManager assetManager;
+    private Texture bombTexture;
+    private Image bombImage;
+
+    public Bomb(){
+        game = TickTackBummGame.getTickTackBummGame();
+
+        assetManager = game.getManager();
+        assetManager.load("assets/bomb/bomb.png",Texture.class);
+        assetManager.finishLoading();
+
+        bombTexture = assetManager.get("assets/bomb/bomb.png",Texture.class);
+        bombImage = new Image(bombTexture);
+    }
+
+
 }
