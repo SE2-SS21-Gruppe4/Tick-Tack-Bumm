bd6e52ee5a1af46bcb6c0dd36997c9c3fad4e2c6
Jusuf <jusuftopic@yahoo.com>
2021-05-19 16:27:16 +0200

    Refactor: Asset manager right implemented

--- core/src/main/java/se2/ticktackbumm/core/models/Bomb.java
+++ core/src/main/java/se2/ticktackbumm/core/models/Bomb.java
@@ -11,15 +11,24 @@ import se2.ticktackbumm.core.TickTackBummGame;
 
 public class Bomb {
 
+    private TickTackBummGame tickTackBummGame;
+    private AssetManager assetManager;
+
     private Stage stage;
 
     private Texture bombTexture;
     private Image bombImage;
 
     public Bomb(){
+        tickTackBummGame = TickTackBummGame.getTickTackBummGame();
+        assetManager = tickTackBummGame.getManager();
+        assetManager.load("bomb/bomb.png",Texture.class);
+        assetManager.finishLoading();
+
         stage = new Stage();
 
-        bombTexture = new Texture("bomb/bomb.png");
+      //  bombTexture = new Texture("bomb/bomb.png");
+        bombTexture = assetManager.get("bomb/bomb.png",Texture.class);
         bombImage = new Image(bombTexture);
 
     }
