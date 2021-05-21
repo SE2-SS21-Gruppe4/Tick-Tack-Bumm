6868417aa0abe8c97a387e1948a0025e0c9d0da9
Jusuf <jusuftopic@yahoo.com>
2021-05-21 14:46:46 +0200

    BombExplosion implemented

--- core/src/main/java/se2/ticktackbumm/core/models/BombImpl/BombExplosion.java
+++ core/src/main/java/se2/ticktackbumm/core/models/BombImpl/BombExplosion.java
@@ -2,6 +2,7 @@ package se2.ticktackbumm.core.models.BombImpl;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 
@@ -29,5 +30,19 @@ public class BombExplosion {
             }
         }
 
+        explosionAnimation = new Animation<TextureRegion>(animationTime/16,textureRegions1D);
+        explosionTime = 0;
+    }
+
+    public void updateExplosion(float delta){
+        explosionTime += delta;
+    }
+
+    public void renderExplosion(SpriteBatch spriteBatch){
+        spriteBatch.draw(explosionAnimation.getKeyFrame(explosionTime), boundsOfExplosion.x,boundsOfExplosion.y,boundsOfExplosion.width,boundsOfExplosion.height);
+    }
+
+    public boolean isFinished(){
+        return explosionAnimation.isAnimationFinished(explosionTime);
     }
 }
