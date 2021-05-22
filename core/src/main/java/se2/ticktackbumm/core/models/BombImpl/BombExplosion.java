8d783f717a6b7c491eb659675a43cacc34f4f77f
Jusuf <jusuftopic@yahoo.com>
2021-05-22 13:24:04 +0200

    fixed split of texture and refactored hardcoded initalization of 1D array

--- core/src/main/java/se2/ticktackbumm/core/models/BombImpl/BombExplosion.java
+++ core/src/main/java/se2/ticktackbumm/core/models/BombImpl/BombExplosion.java
@@ -18,13 +18,13 @@ public class BombExplosion {
         this.boundsOfExplosion = boundsOfExplosion;
 
         //split png from MainScreen
-        TextureRegion[][] textureRegions2D = TextureRegion.split(texture,64,64);
+        TextureRegion[][] textureRegions2D = TextureRegion.split(texture,62,62);
 
         //converted in 1D array
         TextureRegion[] textureRegions1D = new TextureRegion[16];
         int index = 0;
-        for (int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
+        for (int i = 0; i < textureRegions2D.length; i++){
+            for(int j = 0; j < textureRegions2D[0].length; j++){
                 textureRegions1D[index] = textureRegions2D[i][j];
                 index++;
             }
