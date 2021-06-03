9b31995614a1422330c1a27e4b262aac6037203c
Jusuf <jusuftopic@yahoo.com>
2021-06-03 13:43:11 +0200

    got back logic mit arrays and implemented Bitmap font because it is not possible to get word from image

--- core/src/main/java/se2/ticktackbumm/core/models/cards/Card.java
+++ core/src/main/java/se2/ticktackbumm/core/models/cards/Card.java
@@ -4,33 +4,25 @@ package se2.ticktackbumm.core.models.cards;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 import se2.ticktackbumm.core.TickTackBummGame;
 import se2.ticktackbumm.core.data.GameData;
 import se2.ticktackbumm.core.data.GameMode;
 
 import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+
 
 public class Card {
 
-  /*  //TODO delete after implementation with ArrayList
+
     private final String[] vorArray = new String[]{"SCH", "GE", "ANG", "VOR", "SEH", "FRE", "GER", "ACK", "EXP", "ORG"};
     private final String[] middleArray = new String[]{"TER", "UT", "RDI","LEN","ULT","TRA","AHN","KEL","SON","TEN"};
-    private final String[] nachArray = new String[] {"UNG","SCH","SER","KEN","CHE","EIT","ATZ","NER","ICH","TUR"};*/
-
-    private ArrayList<Texture> vorWordsList;
-    private ArrayList<Texture> middleWordsList;
-    private ArrayList<Texture> afterWordsList;
+    private final String[] nachArray = new String[] {"UNG","SCH","SER","KEN","CHE","EIT","ATZ","NER","ICH","TUR"};
 
     private final GameData gameData;
 
@@ -38,8 +30,11 @@ public class Card {
 
     private final Texture frontsideTexture;
 
-
     private boolean isRevealed;
+
+    private BitmapFont font;
+    private String randomWord;
+
 
     //isRevealed moved in GameData class
     //private boolean isRevealed; // TODO: card flip also represented for other players; add to game data?
@@ -47,14 +42,13 @@ public class Card {
     private AssetManager assetManager;
     private TickTackBummGame game;
 
-    private Texture wordFromStack;
 
     private Sprite fontSprite;
     private Sprite backSprite;
 
-
     private Sound openCardSound;
     private Sound cardSchufflingSound;
+
 
     public Card() {
 
@@ -68,9 +62,11 @@ public class Card {
 
         isRevealed = false;
 
-        vorWordsList =getWordsFromAsset("vorWords");
-        middleWordsList = getWordsFromAsset("middleWords");
-        afterWordsList = getWordsFromAsset("afterWords");
+        font = new BitmapFont();
+        font.setColor(Color.DARK_GRAY);
+        font.getData().setScale(4);
+        font.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
 
         assetManager.load("card/backside.png",Texture.class);
         assetManager.finishLoading();
@@ -85,8 +81,8 @@ public class Card {
 
         frontsideTexture = assetManager.get("card/frontside.png",Texture.class);
 
-        wordFromStack = getTextureDepentOnMode();
-        fontSprite = new Sprite(wordFromStack);
+
+        fontSprite = new Sprite(frontsideTexture);
         fontSprite.setBounds(Gdx.graphics.getWidth() / 2.0f - 200, Gdx.graphics.getHeight() / 2.0f, 500, 300);
 
 
@@ -102,7 +98,8 @@ public class Card {
             }
         }
         if (isRevealed){
-            fontSprite.draw(spriteBatch);
+          //  fontSprite.draw(spriteBatch);
+            drawFrontSide(spriteBatch);
 
         }
         else{
@@ -115,30 +112,26 @@ public class Card {
         backSprite.draw(spriteBatch);
     }
 
-
-
-   /* public void drawFontSide(SpriteBatch spriteBatch){
-        Texture randomWord = getTextureDepentOnMode();
-
-        fontSprite = new Sprite(randomWord);
-        fontSprite.setBounds(Gdx.graphics.getWidth() / 2.0f - 200, Gdx.graphics.getHeight() / 2.0f, 500, 300);
-
+    public void drawFrontSide(SpriteBatch spriteBatch){
         fontSprite.draw(spriteBatch);
 
-    }*/
+        randomWord = getWordDependOnMode();
+        font.draw(spriteBatch,randomWord,Gdx.graphics.getWidth() / 2.0f - 100, Gdx.graphics.getHeight() / 2.0f);
+    }
 
 
-    public Texture getTextureDepentOnMode(){
+
+    public String getWordDependOnMode(){
         switch (gameData.getCurrentGameMode()){
             case PREFIX:
-               return getRandomWordFromList(vorWordsList);
+               return getRandomWord(vorArray);
 
             case INFIX:
-                return getRandomWordFromList(middleWordsList);
+                return getRandomWord(middleArray);
 
 
             case POSTFIX:
-              return  getRandomWordFromList(afterWordsList);
+              return  getRandomWord(nachArray);
 
             default:
                 //TODO check if in this case backside should be drawn
@@ -146,40 +139,10 @@ public class Card {
         }
     }
 
-
-    public Texture getRandomWordFromList(ArrayList<Texture> words){
-        Collections.shuffle(words);
-
-        return words.get(0);
+    public String getRandomWord(String[] words){
+        return words[new SecureRandom().nextInt(words.length)];
     }
 
-
-
-
-    public ArrayList<Texture> getWordsFromAsset(String path){
-        ArrayList<Texture> wordsFromAsset = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++){
-            assetManager.load("card/"+path+"/"+i+".png",Texture.class);
-            assetManager.finishLoading();
-
-            Texture wordImage = assetManager.get("card/"+path+"/"+i+".png",Texture.class);
-
-            wordsFromAsset.add(wordImage);
-        }
-
-        return wordsFromAsset;
-    }
-
-
-    //set method in mainscreen to open card every time on new start
-    public void setWordFromStack(Texture texture){
-        this.wordFromStack = wordFromStack;
-    }
-
-    public Texture getWordFromStack(){
-        return this.wordFromStack;
-    }
 
     public boolean isRevealed() {
         return isRevealed;
@@ -189,29 +152,6 @@ public class Card {
         isRevealed = revealed;
     }
 
-    public ArrayList<Texture> getVorWordsList() {
-        return vorWordsList;
-    }
-
-    public void setVorWordsList(ArrayList<Texture> vorWordsList) {
-        this.vorWordsList = vorWordsList;
-    }
-
-    public ArrayList<Texture> getMiddleWordsList() {
-        return middleWordsList;
-    }
-
-    public void setMiddleWordsList(ArrayList<Texture> middleWordsList) {
-        this.middleWordsList = middleWordsList;
-    }
-
-    public ArrayList<Texture> getAfterWordsList() {
-        return afterWordsList;
-    }
-
-    public void setAfterWordsList(ArrayList<Texture> afterWordsList) {
-        this.afterWordsList = afterWordsList;
-    }
 
     public Sprite getFontSprite() {
         return fontSprite;
