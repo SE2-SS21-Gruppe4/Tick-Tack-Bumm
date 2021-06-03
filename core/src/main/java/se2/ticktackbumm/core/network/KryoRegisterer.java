f95019bb435973527c771f846ea9fa602137fd90
Jusuf <jusuftopic@yahoo.com>
2021-06-03 14:36:09 +0200

    CardOpened class registred

--- core/src/main/java/se2/ticktackbumm/core/network/KryoRegisterer.java
+++ core/src/main/java/se2/ticktackbumm/core/network/KryoRegisterer.java
@@ -4,6 +4,7 @@ import com.esotericsoftware.kryo.Kryo;
 import se2.ticktackbumm.core.data.Avatars;
 import se2.ticktackbumm.core.data.GameMode;
 import se2.ticktackbumm.core.network.messages.client.BombExploded;
+import se2.ticktackbumm.core.network.messages.client.CardOpened;
 import se2.ticktackbumm.core.network.messages.client.PlayerReady;
 import se2.ticktackbumm.core.network.messages.client.PlayerTaskCompleted;
 import se2.ticktackbumm.core.network.messages.client.SomeRequest;
@@ -37,6 +38,7 @@ public class KryoRegisterer {
         kryo.register(NextTurn.class);
         kryo.register(NextRound.class);
         kryo.register(GameFinished.class);
+        kryo.register(CardOpened.class);
 
         // register owned classes to send in messages
         kryo.register(Player.class);
