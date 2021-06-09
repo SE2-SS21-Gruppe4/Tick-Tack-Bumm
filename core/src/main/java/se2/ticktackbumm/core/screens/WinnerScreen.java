6502ed4cd3c088d1a964cb64dd9a6f32bffec04a
DanielFrankl <danielfra@edu.aau.at>
2021-06-09 16:39:40 +0200

    take playerlist that is generated from server and use it to get the winner for WinnerScreen implement methods to use the playerlist and call it in WinnerScreen

--- core/src/main/java/se2/ticktackbumm/core/screens/WinnerScreen.java
+++ core/src/main/java/se2/ticktackbumm/core/screens/WinnerScreen.java
@@ -120,6 +120,7 @@ public class WinnerScreen extends ScreenAdapter {
         sprite.setRegionHeight(TickTackBummGame.HEIGHT);
     }
 
+
     public void initTables(Table[] winnerTables) {
         Player player = new Player();
         player = placedPlayers [0];
