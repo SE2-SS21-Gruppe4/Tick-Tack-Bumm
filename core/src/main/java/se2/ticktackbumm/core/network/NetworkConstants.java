91962eda65bcd23cb0e32bfa7e79741cff160f15
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2021-06-14 20:40:24 +0200

    Add documentation to NetworkConstants

--- core/src/main/java/se2/ticktackbumm/core/network/NetworkConstants.java
+++ core/src/main/java/se2/ticktackbumm/core/network/NetworkConstants.java
@@ -1,9 +1,23 @@
 package se2.ticktackbumm.core.network;
 
+/**
+ * Helper class used to encapsulate network information for the game.
+ */
 public class NetworkConstants {
+
+    /**
+     * The TCP port of the game server.
+     */
     public static final int TCP_PORT = 2021;
+    /**
+     * The global request timeout time.
+     */
     public static final int TIMEOUT = 5000;
     //   public static final String HOST_IP = "10.0.2.2";
     //    "10.0.2.2" gives access to the server running on localhost on your machine from the Android emulator
+    /**
+     * The IP address of the game server.
+     */
     public static final String HOST_IP = "127.0.0.1";
+
 }
