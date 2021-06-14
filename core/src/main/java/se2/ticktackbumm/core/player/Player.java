d4329c476dfcdcdbd0e8887a7211c621ab9756ed
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2021-06-14 19:53:21 +0200

    Add documentation to Player and remove unused method

--- core/src/main/java/se2/ticktackbumm/core/player/Player.java
+++ core/src/main/java/se2/ticktackbumm/core/player/Player.java
@@ -2,23 +2,65 @@ package se2.ticktackbumm.core.player;
 
 import com.esotericsoftware.minlog.Log;
 import se2.ticktackbumm.core.data.Avatars;
+import se2.ticktackbumm.core.data.GameData;
+import se2.ticktackbumm.core.screens.WaitingScreen;
 
+/**
+ * The Player class provides all data and methods necessary for the client side of the game.
+ * It holds information about the games players with data like the ID, the game score and the players
+ * name. It is primarily used in the players array of {@link GameData}, to track all players of the game.
+ */
 public class Player {
+
+    /**
+     * The log tag is used to provide unique logging for the class.
+     */
     private static final String LOG_TAG = "PLAYER";
 
+    /**
+     * The connection ID of the player to the server. The connection ID on the server side is incremented
+     * every time a new connection is established and assigned to the player.
+     */
     private int connectionId;
+    /**
+     * The player ID indicates on which position the player is in the players array of
+     * {@link GameData}. Thus you can easily use this ID to retrieve the
+     * player from {@link GameData}.
+     */
     private int playerId;
 
+    /**
+     * The current game score of the player. It indicates how many times the bomb exploded in the players
+     * hands. The higher the score, the worse the player is currently doing.
+     */
     private int gameScore;
+    /**
+     * Indicates whether the player has cheated in the game before. If he has already cheated he can not
+     * cheat again this game.
+     */
     private boolean hasCheated;
 
+    /**
+     * The name the player chose for themself in the {@link WaitingScreen}.
+     */
     private String playerName;
+    /**
+     * The avatar the player chose for themself in the {@link WaitingScreen}.
+     */
     private Avatars playerAvatar;
 
+    /**
+     * Empty default constructor for kryonet and testing.
+     */
     public Player() {
         // kryonet
     }
 
+    /**
+     * @param connectionId the connection ID the server assigned to the player
+     * @param playerId     the position of the player in the players array in
+     *                     {@link GameData}
+     */
     public Player(int connectionId, int playerId) {
         this.connectionId = connectionId;
         this.playerId = playerId;
@@ -26,13 +68,9 @@ public class Player {
         this.hasCheated = false;
     }
 
-    void updatePlayer(Player player) {
-        if (this.connectionId == player.connectionId && this.playerId == player.playerId) {
-            this.setGameScore(player.getGameScore());
-            this.setHasCheated(player.getHasCheated());
-        }
-    }
-
+    /**
+     * Increments the player score, if it is smaller than the max game score.
+     */
     // TODO: refactor to work on server side with max player constant
     public void incPlayerScore() {
         if (gameScore < 10) {
