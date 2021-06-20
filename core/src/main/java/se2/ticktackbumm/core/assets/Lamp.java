f580ce286cac16d824d5e339ceb882bbf432a06d
DanielFrankl <danielfra@edu.aau.at>
2021-06-20 08:43:50 +0200

    documentation for Lamp

--- core/src/main/java/se2/ticktackbumm/core/assets/Lamp.java
+++ core/src/main/java/se2/ticktackbumm/core/assets/Lamp.java
@@ -7,17 +7,33 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import se2.ticktackbumm.core.TickTackBummGame;
 
+/**
+ * Flame is for creating the explosion animation
+ *
+ * @author Daniel Fabian Frankl
+ * @version 1.0
+ */
 public class Lamp {
+    /**
+     * game constants
+     */
     private final TickTackBummGame game;
     private final AssetManager assetManager;
+    /**
+     * animation variables
+     */
     private Animation<TextureRegion> lampAnimation;
     private float animationTime = 0.0f;
-
+    /**
+     * init game constants and get the atlas file from the assetmanager
+     * create the animation
+     */
     public Lamp() {
         this.game = TickTackBummGame.getTickTackBummGame();
         this.assetManager = game.getManager();
 
         TextureAtlas atlas = assetManager.get("lamb.atlas", TextureAtlas.class);
+        //create the animation and give it a framerate
         lampAnimation = new Animation<TextureRegion>(0.2f, atlas.findRegions("lamb"), Animation.PlayMode.LOOP);
     }
 
