1c83a6708ba476bd200a82d210d1ef2eef8a7785
DanielFrankl <danielfra@edu.aau.at>
2021-06-20 08:43:44 +0200

    documentation for Flame

--- core/src/main/java/se2/ticktackbumm/core/assets/Flame.java
+++ core/src/main/java/se2/ticktackbumm/core/assets/Flame.java
@@ -6,18 +6,33 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import se2.ticktackbumm.core.TickTackBummGame;
-
+/**
+ * Flame is for creating the explosion animation
+ *
+ * @author Daniel Fabian Frankl
+ * @version 1.0
+ */
 public class Flame {
+    /**
+     * game constants
+     */
     private final TickTackBummGame game;
     private final AssetManager assetManager;
+    /**
+     * animation variables
+     */
     private Animation<TextureRegion> lambAnimation;
     private float animationTime = 0.0f;
-
+    /**
+     * init game constants and get the atlas file from the assetmanager
+     * create the animation
+     */
     public Flame() {
         this.game = TickTackBummGame.getTickTackBummGame();
         this.assetManager = game.getManager();
 
         TextureAtlas atlas = assetManager.get("flameLoop.atlas", TextureAtlas.class);
+        //create the animation and give it a framerate
         lambAnimation = new Animation<TextureRegion>(0.05f, atlas.findRegions("flameLoop"), Animation.PlayMode.LOOP);
     }
 
